87d46a1e936c42f31f95f075c7d0e25c63bb874b
MatTim314 <edston47@gmail.com>
2023-06-14 00:19:05 +0200

    feat: new pages, navigation abstracted into component

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<div>
+  <a href="/">Home</a>
+  <a href="/about/">About</a>
+  <a href="/blog/">Blog</a>
+</div>
+
+<style>
+    div {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        place-items: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+        padding: 2rem;
+        font-size: 1.5rem;
+    }
+</style>
