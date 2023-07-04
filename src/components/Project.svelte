62f3ac7b524ceab1eea5f00aff2bea055f3b203f
MatTim314 <edston47@gmail.com>
2023-07-05 01:41:55 +0200

    feat: project page

---
+++ src/components/Project.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  export let description: string;
+  export let name: string;
+  export let imageSource: string;
+</script>
+
+<div class="project">
+  <div class="left-side">
+    <span class="name">{name}</span>
+    <span class="description">{description}</span>
+  </div>
+  <div class="right-side">
+    <img src={imageSource} alt="" />
+  </div>
+</div>
+
+<style>
+
+    .project:hover{
+        transform: translate(0, -3px);
+        background-color: #172033;
+        transition: background-color 150ms ease-in, transform 150ms ease-in-out;;
+    }
+
+  .project {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    place-items: stretch;
+    gap: 1rem;
+
+    background-color: #0E131F;
+    border-radius: 10px;
+    width: 50%;
+    cursor: pointer;
+  }
+  .description {
+    color: #92828D;
+  }
+  .name {
+    font-size: 2rem;
+    font-family: inherit;
+  }
+  .left-side{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    gap: 5rem;
+    margin: 1rem;
+    padding: 0.5rem;
+  }
+  .right-side{
+    display: flex;
+    justify-content: center;
+    margin: 1rem;
+  }
+  .right-side >  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    outline: 1px #92828D solid;
+    border-radius: 5px;
+    margin-right: 1rem;
+  }
+
+
+</style>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Project from "./Project.svelte";
+</script>
+
+<div class="projects">
+    
+<Project 
+      name={"To build a fire"}
+      description={"This was a school project done in C++ from scratch using only OpenGL library. Main purpose was to get familiar with rendering methods, lightning models and optimalization."}
+      imageSource={'/lk.gif'}/>
+      <Project 
+      name={"Acolyte"}
+      description={"Simple side project done in Python using tkinter library. Idea was to simulate playing Dota 2's Invoker hero"}
+      imageSource={'/lk.gif'}/>
+      <Project 
+      name={"To build a fire"}
+      description={"This was a school project done in C++ from scratch using only OpenGL library. Main purpose was to get familiar with rendering methods, lightning models and optimalization."}
+      imageSource={'/lk.gif'}/>
+    
+</div>
+
+<style>
+    .projects{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+    }
+</style>
