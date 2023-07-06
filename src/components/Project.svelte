a810b1b425b6ad8061d35c09db45e58ca8695740
MatTim314 <edston47@gmail.com>
2023-07-06 13:09:52 +0200

    feat: static covers for gifs, click redirect to github pages

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -2,20 +2,36 @@
   export let description: string;
   export let name: string;
   export let imageSource: string;
+  export let coverSource: string;
+  export let link: string
+
+
+
+  function redirect(){
+    window.location.assign(link)
+  }
 </script>
 
-<div class="project">
+<div class="project" style="cursor: pointer;" on:click={ redirect} on:keydown={redirect}>
   <div class="left-side">
     <span class="name">{name}</span>
     <span class="description">{description}</span>
   </div>
   <div class="right-side">
     <img src={imageSource} alt="" />
+    <img class="cover" src={coverSource} alt="" />
   </div>
 </div>
 
-<style>
 
+<style>
+  .cover {
+    position: absolute;
+    object-position: 0 0;
+  }
+  .cover:hover {
+    opacity: 0;
+  }
     .project:hover{
         transform: translate(0, -3px);
         background-color: #172033;
@@ -49,17 +65,19 @@
     padding: 0.5rem;
   }
   .right-side{
+    position: relative;
     display: flex;
     justify-content: center;
     margin: 1rem;
   }
   .right-side >  img {
+    flex-shrink: 0;
     width: 100%;
     height: 100%;
     object-fit: cover;
     outline: 1px #92828D solid;
     border-radius: 5px;
-    margin-right: 1rem;
+    margin-right: 2rem;
   }
 
 
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -5,17 +5,25 @@
 <div class="projects">
     
 <Project 
-      name={"To build a fire"}
-      description={"This was a school project done in C++ from scratch using only OpenGL library. Main purpose was to get familiar with rendering methods, lightning models and optimalization."}
-      imageSource={'/lk.gif'}/>
+      name={"To Build a Fire"}
+      description={"This was a school project done in C++ from scratch using only OpenGL library. Main purpose was to get familiar with rendering methods, lightning models and optimalization. Result was a glooming game about not freezing to death."}
+      imageSource={'/lk.gif'}
+      coverSource={'/fire-cover.png'}
+      link={""}/>
+
       <Project 
       name={"Acolyte"}
-      description={"Simple side project done in Python using tkinter library. Idea was to simulate playing Dota 2's Invoker hero"}
-      imageSource={'/lk.gif'}/>
+      description={"Simple side project done in Python using tkinter library. Idea was to simulate playing Dota 2's Invoker hero."}
+      imageSource={'/acolyte.gif'}
+      coverSource={'/acolyte-cover.png'}
+      link={"https://github.com/MatTim314/acolyte"}/>
+
       <Project 
-      name={"To build a fire"}
-      description={"This was a school project done in C++ from scratch using only OpenGL library. Main purpose was to get familiar with rendering methods, lightning models and optimalization."}
-      imageSource={'/lk.gif'}/>
+      name={"Password Generator"}
+      description={"Project done in Python for generating strong passwords. "}
+      imageSource={'/passwords.gif'}
+      coverSource={'/password-cover.png'}
+      link={"https://github.com/MatTim314/pswrd_gen"}/>
     
 </div>
 
