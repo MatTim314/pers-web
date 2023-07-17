798ff830e10decc8534ae20baa664cde1db826ae
MatTim314 <edston47@gmail.com>
2023-07-17 02:37:13 +0200

    feat: introduction section is animated

--- src/components/Introduction.svelte
+++ src/components/Introduction.svelte
@@ -1,72 +1,88 @@
 <script lang="ts">
- let inTouch : string = ''
- let contact : string = 'edston47@gmail.com'
- let touched : boolean = false;
+  let inTouch: string = "";
+  let contact: string = "edston47@gmail.com";
+  let touched: boolean = false;
 
- function whenTouched(){
-    touched = !touched
-    inTouch = touched ? contact : ''
- }
+  function whenTouched() {
+    touched = !touched;
+    inTouch = touched ? contact : "";
+  }
 </script>
 
-
-
-
-<div>
-    Hi, I'm <span class="name">Matej</span> <span class="subtle italic">[mah-tey]</span>
-<p>Nice to meet you</p>
+<div class="outer">
+<div class="first-line">
+    Hi, I'm <span class="name">Matej</span>
+  <span class="subtle italic">[mah-tey]</span>
 </div>
+  <p>Nice to meet you</p>
+</div>
+
 <style>
+  .name {
+    cursor: pointer;
+    color: var(--accent-yellow-color);
+  }
+  .name:hover + .italic,
+  .name + .italic {
+    transition: opacity 500ms ease;
+  }
 
-    .name {
-        cursor: pointer;
-        color: var(--accent-yellow-color)
-    }
-    .name:hover +.italic,
-    .name +.italic{
-        transition: opacity 500ms ease;
-    }
+  .name:hover + .italic {
+    opacity: 1;
+  }
+  .name + .italic {
+    opacity: 0;
+  }
 
-    .name:hover + .italic {
-        opacity: 1;
-    }
-    .name + .italic {
-        opacity: 0;
-    }
+  p {
+    position: relative;
+    max-width: fit-content;
+    transition: opacity 500ms ease;
+    animation: fadeIn 2s;
 
-    p {
-        position: relative;
-        max-width: fit-content;
-    }
+  }
 
-    p::before {
-        content: '';
-        position: absolute;
-        bottom: -3px;
-        width: 0%;
-        border-bottom: 1px white solid;
-        transition: width 1s ease;
-    }
-    p:hover::before{
-        border-bottom-color: var(--accent-yellow-color);
-        right: 0;
-        width: 100%;
-    }
-    
-    
-        
-    .italic {
-        font-style: italic;
-    }
+  p::before {
+    content: "";
+    position: absolute;
+    bottom: -3px;
+    width: 0%;
+    border-bottom: 1px var(--accent-yellow-color) solid;
+    transition: width 350ms ease;
+  }
+  p:hover::before {
+    border-bottom-color: var(--accent-yellow-color);
+    right: 0;
+    width: 100%;
+  }
 
-    .subtle {
-        color: grey;
-        font-size: 2.5rem;
-    }
+  .italic {
+    font-style: italic;
+  }
 
-    div {
-        font-size: 3rem;
-        margin-top: 40vh;
-        margin-bottom: 40vh;
+  .subtle {
+    color: var(--main-background-color);
+    font-size: 2.5rem;
+  }
+
+  .first-line{
+    animation: fadeIn 2s;
+  }
+  .outer {
+    font-size: 3rem;
+    margin-top: 40vh;
+    margin-bottom: 40vh;
+    width: 50%;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(100%);
     }
+    100% {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
 </style>
