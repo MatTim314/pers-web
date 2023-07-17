78e53dd1a0546fd2abe6c7ce3f4fddefddc3e43a
MatTim314 <edston47@gmail.com>
2023-07-17 02:36:43 +0200

    feat: CV section rework

--- src/components/CvDownload.svelte
+++ src/components/CvDownload.svelte
@@ -5,21 +5,52 @@
     }
 </script>
 
-<div>
-    <h1>CV</h1>
-    <p>If my professional experience is of any interest to you feel free to <a on:mouseenter={recolor} href="../../public/CV-eng-IT.pdf">download my CV</a></p>
+<div class="outer">
+    
+    <div class="header"></div>
+    <p>I'm a software engineer with a thing for design. I enjoy creating useful tools that also look good.</p>
+    <p>Feel free to <a class="dw-button" on:mouseenter={recolor} href="../../public/CV-eng-IT.pdf">download my CV.</a></p>
+    <div class="image-overlay"></div>
     
 </div>
 
 <style>
-    div {
-        outline: 1px red solid;
+    .outer {
+        position: relative;
         width: 50%;
         display: flex;
         flex-direction: column;
         place-items: center;
     }
 
+    .image-overlay {
+        position: absolute;
+        width: 50%;
+        height: 150%;
+        opacity: 0;
+        border-radius: 10px;
+        right: 0;
+        top: 7rem;
+        background-image: url(../../public/cv-preview.png);
+        background-position: 0 0;
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        z-index: -1;
+        transition: opacity 300ms ease, box-shadow 300ms ease;
+    }
+    .image-overlay:hover{
+        opacity: 0.3;
+        box-shadow: 0 0 10px white;
+    }
+    .header {
+        align-self: flex-start;
+        font-size: 3rem;
+    }
+    
+    p {
+        align-self: flex-start;
+        font-size: 2rem;
+    }
     a {
         text-decoration: none;
         color: inherit;
@@ -28,18 +59,19 @@
 
     a:hover {
         color: var(--accent-red-color);
-        color: red;
     }
 
     a::before {
         content: ' ';
         position: absolute;
         bottom: -3px;
-        width: 100%;
+        width: 0%;
         border-bottom: 1px white solid;
+        transition: width 200ms ease;
     }
-    a::before:hover{
-        border-bottom: 1px red solid;
+    a:hover::before{
+        border-bottom: 1px var(--accent-red-color) solid;
+        width: 100%;
     }
 
 </style>
