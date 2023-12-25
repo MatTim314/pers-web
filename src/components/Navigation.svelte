53e8b324987e647ca5a29a6899d691158b4ae152
MatTim314 <edston47@gmail.com>
2023-12-26 00:39:39 +0100

    fix: cleanup

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,35 +1,23 @@
 <script lang="ts">
-    
 </script>
 
 <div>
-    <div>
-    <a href="/">Home</a>
-    <a href="/about/">About</a>
-    <a href="/projects/">Projects</a>
-    <a href="/timers/">Timers</a>
-    </div>
+  <a href="/">CV</a>
+  <a href="/projects/">Projects</a>
+  <a href="/timers/">Timers</a>
 </div>
 
-
 <style>
-    div {
-        display: flex;
-        justify-content: center;
-    }
-    div > div{
-        align-self: center;
-        width: 50%;
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        justify-items: center;
-    }
-
-    a {
-        font-family: inherit;
-        text-decoration: none;
-        color: inherit;
-        padding: 2rem;
-        font-size: 1.5rem;
-    }
+  div {
+    gap: 1rem;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+  }
+  a {
+    font-family: inherit;
+    text-decoration: none;
+    color: inherit;
+    font-size: 3rem;
+  }
 </style>
